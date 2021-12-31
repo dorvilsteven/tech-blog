38c6a70b3c68015bbe4d87fc14c15bbbab745c08
StevenD <dorvilsteven@gmail.com>
2021-12-31 00:56:09 -0500

    create mysql database and connection, .env variables, and both users and post models

---
+++ models/Post.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const db = require('../config/connection');
+
+class Post extends Model {
+
+}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        created_date: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        }
+    },
+    {
+        db,
+        timestamps: true,
+        createdAt: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Post',
+        tableName: 'posts'
+    }
+);
+
+module.exports = Post;
---
+++ models/index.js
@@ -0,0 +1,6 @@
+// import models
+const User = require("./User");
+const Post = require("./Post");
+
+// export models
+module.exports = { User, Post };
