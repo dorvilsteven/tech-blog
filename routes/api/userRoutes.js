5da340a7f4ca81b186145852a1410d41c8e43f51
StevenD <dorvilsteven@gmail.com>
2022-01-08 22:10:29 -0500

    logout link

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -85,17 +85,6 @@ router.post('/login', (req, res) => {
     });
 });
 
-router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
-    }
-    else {
-        res.status(404).end();
-    }
-});
-
 router.delete('/:id', (req, res) => {
     User.destroy({
         where: {
